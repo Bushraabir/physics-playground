d8ba621f16395abb3e039a637bd427bf9d220777
bushraabir <muzahidulislamabir123@gmail.com>
2025-01-28 12:46:23 +0600

    fix link path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/apply-forces': 'http://localhost:8000',
-        },
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/apply-forces': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
     },
-    base:'/physics-playground/'
-})
+  },
+ 
+});
